100ec11d0a0aa131161fccb322eeda5f2627fcff
farhad-zada <113878666+farhad-zada@users.noreply.github.com>
2022-12-19 12:17:12 +0400

    Update GetTube.py

--- GetTube.py
+++ GetTube.py
@@ -1,3 +1,15 @@
+""" 
+You just put the url of youtube video, choose quality and select 
+a name for file then the code executes and the given video downloaded.
+
+Check out this article if you can't figure out what is happening:
+https://subhadip.ca/information-technology/scraping-youtube-with-beautifulsoup-and-python3/
+
+I also would like to admit that i haven't put some if-else and try-except 
+statements to handle possible errors. If you want to use this code you can simply add them.
+"""
+
+
 import requests as rs
 from bs4 import BeautifulSoup as bs
 import json as js
@@ -13,13 +25,11 @@ class GetTube:
     fhand = open(self.fname + ".mp4", 'wb')
     [fhand.write(chunk) for chunk in content.iter_content(chunk_size = 1024**2) if chunk]
     fhand.close()
+    
+# The below code is up to you, you can modify it anyway you want, and add some conditions to handle errors
 
 GetTube(
     input('URL: '),
     int(input( 'For desired quality choose a number\nHigh:\t0\nMed:\t2\nLow:\t4\n')), 
     input("Type in desired file name (don't add extension): ")
     ).download()
-
-
-# Used : 
-'https://subhadip.ca/information-technology/scraping-youtube-with-beautifulsoup-and-python3/'
